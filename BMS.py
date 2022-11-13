b3390e318e17420311fb920ce96d3727e87d7503
Enzo-1098 <113872677+Enzo-1098@users.noreply.github.com>
2022-11-13 16:19:52 +0800

    Update BMS.py

--- BMS.py
+++ BMS.py
@@ -1,4 +1,9 @@
-def BMS_expand(S, n):
+def BMS_compare(a, b):
+    for i in range(len(a)):
+        if a[i] != b[i]: return a[i] < b[i]
+    return False
+
+def BMS_expand(seq, n):
     # rule 1 not included
-    if all(i == 0 for i in S[-1]): return S[:-1] # rule 2
-    
+    if all(i == 0 for i in seq[-1]): return seq[:-1] # rule 2
+    # rule 3.1
